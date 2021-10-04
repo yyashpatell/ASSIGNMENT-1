4442bc94bf3839a679ff41ba2d1efa898a95cf69
yyashpatell <90631590+yyashpatell@users.noreply.github.com>
2021-10-04 10:47:12 -0400

    Add files via upload

---
+++ achievement #3/Yash_Patel_8736546_ACH3.py
@@ -0,0 +1,12 @@
+sum=0   
+
+n = int(input("enter the LIMIT:")) 
+
+i=2
+
+while i<=n: 
+
+    sum=sum+(i*i)
+    i=i+2
+
+print("the total sum of squares of EVEN number upto the entered LIMIT is:",sum)
